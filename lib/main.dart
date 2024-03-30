b0ef76c75d8540a9819c02805aae4ad8d7e44ad1
Aleksey Kuzmin <kboomska@gmail.com>
2024-03-30 10:48:43 +0300

    Init application

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello',
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+    );
+  }
+}
